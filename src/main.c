abc92c1e8c384fae746dffcaac080abe1dfdd22a
Prichman <prichmen@gmail.com>
2016-07-15 14:13:59 +0600

    Added opportunity to remove symbols in open/save subwindows

--- src/main.c
+++ src/main.c
@@ -82,6 +82,7 @@ int OpenFile(WINDOW *text_window)
   /*  Creating the window*/
   open_window = newwin(10, 30, 6, 15);
   open_subwindow = derwin(open_window, 8, 28, 1, 1);
+  keypad(open_subwindow, 1);
   box(open_window, 0, 0);
 
   wrefresh(open_window);
@@ -90,9 +91,17 @@ int OpenFile(WINDOW *text_window)
   /*  Logic */
   mvwprintw(open_subwindow, 0, 0, "Please, write filename");
   wmove(open_subwindow, 1, 0);
-  while ((ch = wgetch(open_subwindow)) != '\n') {
-    waddch(open_subwindow, ch);
-    file_name[length++] = ch;
+  while (1) {
+    ch = wgetch(open_subwindow);
+    if (ch  == '\n') {
+      break;
+    } else if (isalpha(ch)) {
+      waddch(open_subwindow, ch);
+      file_name[length++] = ch;
+    } else if (ch == KEY_BACKSPACE) {
+      --length;
+      mvwdelch(open_subwindow, 1, length);
+    }
   }
   file_name[length] = '\0';
   if ((fd = open(file_name, O_RDWR, 0666)) == -1) {
@@ -148,7 +157,8 @@ int SaveFile(WINDOW *text_window, int max_y)
 
   save_window = newwin(10, 30, 6, 15);
   save_subwindow = derwin(save_window, 8, 28, 1, 1);
-  box(save_subwindow, 0, 0);
+  keypad(save_subwindow, 1);
+  box(save_window, 0, 0);
 
   wrefresh(save_window);
   wrefresh(save_subwindow);
@@ -156,24 +166,36 @@ int SaveFile(WINDOW *text_window, int max_y)
   /*  Logic */
   mvwprintw(save_subwindow, 0, 0, "Please, write filename");
   wmove(save_subwindow, 1, 0);
-
-  while ((ch = wgetch(save_subwindow)) != '\n') {
-    waddch(save_subwindow, ch);
-    file_name[length++] = ch;
+  while (1) {
+    ch = wgetch(save_subwindow);
+    if (ch == '\n') {
+      break;
+    } else if (isalpha(ch)) {
+      waddch(save_subwindow, ch);
+      file_name[length++] = ch;
+    } else if (ch == '\a') {
+      /*  There is have to be (ch == KEY_BACKSPACE), but
+       *  it's now working and I don't know why. It works
+       *  in OpenFile, but not here*/
+      --length;
+      mvwdelch(save_subwindow, 1, length);
+    }
   }
   file_name[length] = '\0';
-  if ((fd = open(file_name, O_WRONLY, 0666)) == -1) { 
-    mvwprintw(save_subwindow, 0, 0, "Can't open the file!!!");
-    mvwprintw(save_subwindow, 1, 0, "Press any key to continue");
-    curs_set(0);
-    error_found = 1;
-    file_name[0] = '\0';
+  if ((fd = open(file_name, O_CREAT | O_EXCL | O_WRONLY, 0666)) == -1) {
+    if ((fd = open(file_name, O_WRONLY, 0666)) == -1) { 
+      mvwprintw(save_subwindow, 0, 0, "Can't open the file!");
+      mvwprintw(save_subwindow, 1, 0, "Press any key to continue");
+      curs_set(0);
+      error_found = 1;
+      file_name[0] = '\0';
+    }
   }
-  else {
+  if (!error_found) {
     for (current_line = 0; current_line < max_y; ++current_line) {
       mvwinnstr(text_window, current_line, 0, line, 79);
       if (write(fd, line, 79) == -1) { 
-        mvwprintw(save_subwindow, 0, 0, "Can't write into the file!!!");
+        mvwprintw(save_subwindow, 0, 0, "Can't write into the file!");
         mvwprintw(save_subwindow, 1, 0, "Press any key to continue");
         curs_set(0);
         error_found = 1;
@@ -182,18 +204,16 @@ int SaveFile(WINDOW *text_window, int max_y)
 
     close(fd);
   }
+  wmove(text_window, current_y, current_x);
   if (error_found) {
     wgetch(save_subwindow);
     curs_set(1);
   }
-  wmove(text_window, current_y, current_x);
   delwin(save_subwindow);
   delwin(save_window);
 
-  return -error_found;
-  
+  return -error_found; 
 }
-
 
 void GetNextVerticalPos(WINDOW *text_window, enum Direction dir, int *y, int *x,
                        int delta)
@@ -267,6 +287,9 @@ void RemoveChar(WINDOW *text_window)
   getyx(text_window, current_y, current_x);
   if (current_x != 0)
     mvwdelch(text_window, current_y, current_x - 1);
+  else if (current_y != 0) {
+    mvwdelch(text_window, current_y - 1, 78); 
+  }
 }
 
 int main(void)
@@ -285,17 +308,17 @@ int main(void)
   initialize();
   current_x = 0;
   current_y = 0;
-  last_y = 0;
+  last_y = 1;
 
   /* ====================================== */
   /*                                        */
   /*  Creating all windows                  */
   /*                                        */
   text_window = newwin(TEXT_WINDOW_HEIGHT, TEXT_WINDOW_WIDTH, 0, 0);
-  text_subwindow = subwin(text_window, 10,
-                          10, 1, 1);
-  /* text_subwindow = subwin(text_window, TEXT_WINDOW_HEIGHT - 2,
-                          TEXT_WINDOW_WIDTH - 2, 1, 1); */
+  /* text_subwindow = subwin(text_window, 10,
+                          10, 1, 1); */
+  text_subwindow = subwin(text_window, TEXT_WINDOW_HEIGHT - 2,
+                          TEXT_WINDOW_WIDTH - 2, 1, 1);
   keypad(text_subwindow, 1);
   scrollok(text_subwindow, 1);
   idlok(text_subwindow, 1);
@@ -314,6 +337,9 @@ int main(void)
     symbol = wgetch(text_subwindow); 
     if (isalpha(symbol)) {
       waddch(text_subwindow, symbol);
+      getyx(text_subwindow, current_y, current_x);
+      if (current_x == 0)
+        ++last_y;
     } else {
       switch (symbol) {
         case KEY_F(2):
@@ -324,7 +350,9 @@ int main(void)
           curs_set(1);
           break;
         case KEY_F(3):
-          SaveFile(text_subwindow, last_y);
+          if (SaveFile(text_subwindow, last_y) != -1) {
+            redrawwin(text_subwindow);
+          }
           break;
         case KEY_F(4):
           /*  TODO: find */
